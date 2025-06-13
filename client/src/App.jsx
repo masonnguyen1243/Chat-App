a9a380f674fea5994fc8db01de116a802961bb96
cuongnguyenba1243 <2121050937@student.humg.edu.vn>
2025-06-13 13:12:35 +0700

    Setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "~/pages/HomePage";
+import LoginPage from "~/pages/LoginPage";
+import ProfilePage from "~/pages/ProfilePage";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-green-500 underline">
-        Hello world!
-      </h1>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <div>
+    <div className="h-screen w-full border sm:px-[15%] sm:py-[5%]">
       <h1>HomePage</h1>
     </div>
   );
